e2c3a62f64199e625a7fe537c457006d6f0f33b7
uni51 <unieye51@gmail.com>
2021-03-29 14:54:58 +0900

    5-2: チャットルーム登録機能の作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <div>
+      <div
+        v-for="room in rooms"
+        :key="room.id"
+        class="bg-white max-w-sm rounded-lg overflow-hidden shadow m-4 mb-5 p-4 h-32"
+      >
+        <div>
+          <img
+            :src="room.topImageUrl"
+            class="float-left object-cover rounded-lg w-24 h-24 mr-4"
+          />
+          <p class="font-mono text-darkGray">{{ room.name }}</p>
+        </div>
+      </div>
+    </div>    
     <!-- ルーム作成ボタン -->
     <div class="fixed flex justify-end bottom-0 w-full max-w-sm">
       <button @click="openModal">
@@ -16,12 +31,13 @@
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
 import ModalBase from '~/components/ModalBase'
 import CreateRoomModal from '~/components/CreateRoomModal'
 
 export default {
   middleware: ['checkAuth'],
-  
+
   components: {
     ModalBase,
     CreateRoomModal
@@ -29,10 +45,24 @@ export default {
 
   data() {
     return {
-      isCreateMode: false
+      isCreateMode: false,
+      unsubscribe: null
     }
   },
-
+  computed: {
+    ...mapGetters('rooms', ['rooms'])
+  },  
+  async asyncData({ store }) {
+    const unsubscribe = await store.dispatch('rooms/subscribe')
+    return {
+      unsubscribe
+    }
+  },
+  // Vue インスタンスが破棄された後に呼ばれる destroyed というライフサイクル内で処理を行う
+  destroyed() {
+    this.$store.dispatch('rooms/clear')
+    if (this.unsubscribe) this.unsubscribe()
+  },
   methods: {
     openModal() {
       this.isCreateMode = true
